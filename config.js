1f32b76b3deb45c70342c570dba6bc777ddc5e0b
dungtq <quangdung99bg@gmail.com>
2021-10-07 11:00:35 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hey my Babe,'+'<br>'+' Hặn lươn xinh gái ❤❤❤',
+    introDesc: `Ò ó o… cạc cạc... chích chích... gâu gâu.
+    Trời đã sáng rồi dậy thôi nào babee :<
+    Đêm qua bae ngủ ngon hông?
+    Tặng em món quả nho nhỏ mang đầy yêu thương của anh, chúc babe của anh ngày mới tốt lành. Love you😘😘😘 `,
+    btnIntro: 'Bấm đây để zô ❤ anh nè',
+    title: 'Em có biết trong anh em là số mấy hôngggg?😙',
+    desc: 'Em là 121247 ! Vì sao ư? Vì em là một nửa của anh ❤  ',
+    btnYes: 'Vâng. Vẫn cứ là thích anh ❤',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chúc babe của anh ngày mới vui vẻ, tràn đầy năng lượng nhé. Yêu em ❤.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://c.tenor.com/G4Fevj6hOgIAAAAC/love3000-3000.gif' 
+}
